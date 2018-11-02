036be704ea6cc526e6c708b06727c26dedaf49f1
zytwh2012 <zytwh2012@gmail.com>
2018-11-01 21:27:43 -0600

    first

---
+++ shareForm/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule,} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { PostComponent } from './post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ shareForm/src/app/post/post.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPost } from './post';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private _post_url:string =  "/asserts/post.json"
+
+  constructor(private http:HttpClient) { }
+
+  getPost():Observable<IPost[]>{
+    return this.http.get<IPost[]>(this._post_url)
+  }
+}
