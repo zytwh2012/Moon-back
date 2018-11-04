02ba912c74c0c99eda1a1c3759294c27ef9b9b82
zytwh2012 <zytwh2012@gmail.com>
2018-11-03 23:01:03 -0600

    login-fix

--- shareForm/src/app/app.module.ts
+++ shareForm/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,10 +18,12 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
