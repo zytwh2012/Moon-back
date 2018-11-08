867c85113ebb7f278fba9cddde8f25f893a26be1
zytwh2012 <zytwh2012@gmail.com>
2018-11-08 01:06:46 -0700

    register partly works

--- shareForm/server/routes/api.js
+++ shareForm/server/routes/api.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = require('../models/user')
+
+const db = 'mongodb://yz:qaz98765432@ds155213.mlab.com:55213/db1'
+
+mongoose.connect(db, { useNewUrlParser: true },error =>{
+    if(error){
+        console.error(error)
+    }else{
+        console.log("connected")
+    }
+})
 
 router.get('/', (req, res) =>{
     res.send('From API route')
 })
 
+router.post('/register', (req, res) =>{
+    let userData = req.body
+    let user = new User(userData)
+    user.save(error,regesitedUser =>{
+        if(error){
+            console.log(error)
+        }else{
+            res.status(200).send(regesitedUser)
+        }
+    })
+})
 module.exports = router
