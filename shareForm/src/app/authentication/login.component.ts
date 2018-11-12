14dbb6858038d631581e46902af01fe4b2e745e0
haotianzhu <zhuhaotian19970215@icloud.com>
2018-11-12 16:04:25 -0700

    # <type>: (If applied, this commit will...) <subject> (Max 50 char) # |<----  Using a Maximum Of 50 Characters  ---->| add login branch to master
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring production code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    docs     (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    chore    (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Capitalize the subject line
    #    Use the imperative mood in the subject line
    #    Do not end the subject line with a period
    #    Separate subject from body with a blank line
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------

--- shareForm/src/app/authentication/login.component.ts
+++ shareForm/src/app/authentication/login.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { RegistrareService } from './registrate.service';
 import { LoginService } from './login.service';
+declare var $: any;
 
 @Component({
   selector: 'app-login',
@@ -10,9 +11,9 @@ import { LoginService } from './login.service';
   styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent implements OnInit {
-  loginInRequestData = {};
-  signUpRequestData = {};
+export class LoginComponent implements OnInit, AfterViewInit {
+  loginInRequestData = {email: '', password: ''};
+  signUpRequestData = {email: '', password: ''};
   private is_login_hidden = false ;
 
   constructor(private router: Router, private location: Location, private _reg: RegistrareService, private _log: LoginService) { }
@@ -27,19 +28,42 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  ngAfterViewInit(): void {
+    // jquery to avoid user typing space to username
+    $('#inputUsername').on({
+      keydown: function(e) {
+        if (e.which === 32) {
+          return false;
+        }
+      },
+      change: function() {
+        this.value = this.value.replace(/\s/g, '');
+      }
+    });
+  }
+
   logIn() {
-    console.log(this.loginInRequestData),
-    this._log.loginInRequest(this.loginInRequestData)
+    console.log(this.loginInRequestData);
+
+    if (this.signUpRequestData.email && this.signUpRequestData.password) {
+      this._log.loginInRequest(this.loginInRequestData)
       .subscribe(
       res => console.log(res),
       error => console.log(error)
-    );
-  }
-  singUp() {
-    this._reg.signUpRequest(this.signUpRequestData)
-      .subscribe(
-        res => console.log(res),
-        error => console.log(error)
       );
+      this.router.navigate(['/']);
+    }
   }
+
+  singUp() {
+    if (this.signUpRequestData.email && this.signUpRequestData.password) {
+      this._reg.signUpRequest(this.signUpRequestData)
+        .subscribe(
+          res => console.log(res),
+          error => console.log(error)
+        );
+        this.is_login_hidden = false;
+      }
+  }
+
 }
