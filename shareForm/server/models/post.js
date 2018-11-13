5275f85134bd4f75a5d94d88b814452be7fc2576
zytwh2012 <zytwh2012@gmail.com>
2018-11-12 20:13:46 -0700

    fixed

--- shareForm/server/models/post.js
+++ shareForm/server/models/post.js
@@ -2,8 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var postSchema = new Schema({
-  id:  'string',
-  password: 'string',
+  id: Number,
+  post_owner_id: Number,
+  parent_post: Number,
+  child_post: Number,
+  post_content: String,
+  title: String,
+  branch: String,
+  last_edited: Date
+
 });
 
-module.exports = mongoose.model('post',podtSchema,'posts');
+module.exports = mongoose.model('post',postSchema,'posts');
--- shareForm/server/routes/api.js
+++ shareForm/server/routes/api.js
@@ -46,19 +46,62 @@ router.post('/login', (req, res) => {
       }
     })
   })
-  
-  router.post('/register', (req, res) =>{
 
-    let userData = req.body;
-    let user = new User(userData);
+  // post
+  router.post('/post', (req, res) =>{
+    let postData = req.body;
+    let post = new Post(postData);
    
-    user.save((error,regesitedUser) =>{
+    post.save((error,postedPost) =>{
         if(error){
             console.log(error)
         }else{
-            res.status(200).send(regesitedUser)
+            res.status(200).send(postedPost)
         }
     })
 })
 
+// // main page feed
+// router.post('/feed', (req, res) =>{
+
+//     let pullRequest = req.body;
+
+//     Post.find()
+//         .sort({'last_edited': -1})
+//         .limit(10)
+//         .exec((err, user) => {
+//         if (err) {
+//           console.log(err)    
+//         } else {
+
+//         }
+
+//     })
+
+// // branch feed
+// router.post('/branch_feed', (req, res) =>{
+
+//     let pullRequest = req.body;
+//     Post.find({email: userData.email}, (err, user) => {
+//         if (err) {
+//           console.log(err)    
+//         } else {
+
+//         }
+
+//     })
+
+// router.post('/pull', (req, res) =>{
+
+//     let userData = req.body;
+//     let user = new User(userData);
+   
+//     user.save((error,regesitedUser) =>{
+//         if(error){
+//             console.log(error)
+//         }else{
+//             res.status(200).send(regesitedUser)
+//         }
+//     })
+// })
 module.exports = router
