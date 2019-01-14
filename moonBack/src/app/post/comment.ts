e186a86a565f03222e6928c357a199c908e4ea70
haotianzhu <zhuhaotian19970215@icloud.com>
2019-01-13 21:37:21 -0700

    feat: add comment.ts ——————————————————————————————————————     feat     (new feature)     fix      (bug fix)     refactor (refactoring production code)     style    (formatting, missing semi colons, etc; no code change)     docs     (changes to documentation)     test     (adding or refactoring tests; no production code change)     chore    (updating grunt tasks etc; no production code change) ——————————————————————————————————————

---
+++ moonBack/src/app/post/comment.ts
@@ -0,0 +1,23 @@
+import {Post} from './post';
+
+export class Comment {
+    id: string | Int32Array;
+    commentOwnerId: string;
+    commentContent: object;
+    lastEdited: number;
+    tags: [string];
+    children: [Comment];
+    parent: [Comment|Post];
+    root: [Post];
+
+    constructor(
+        id: string | Int32Array,
+        commentOwnerId: string,
+        commentContent: object,
+        lastEdited: number,
+        tags: [string],
+        children: [Comment],
+        parent: [Comment|Post],
+        root: [Post],
+    ) {}
+}
